55c6f482bbb4e3d63185bebf55265254afb38148
Your Name <you@example.com>
2025-05-14 22:33:18 -0400

    Added Render domain to ALLOWED_HOSTS

--- django_backend/myproject/settings.py
+++ django_backend/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m%(ihdl-eoyk9bv)_v*k)68^8uol4wh(sre-14gj3=q=piv=9o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['capstone-car-dealer.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
